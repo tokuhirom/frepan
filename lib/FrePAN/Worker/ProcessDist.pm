6173dd70b63b362c9598a0312d0de185283efaa8
tokuhirom <tokuhirom@gmail.com>
2010-03-08 11:34:57 +0900

    ignore sample dir

--- lib/FrePAN/Worker/ProcessDist.pm
+++ lib/FrePAN/Worker/ProcessDist.pm
@@ -83,7 +83,7 @@ sub run {
                 # msg("skip $f, by $no_index");
                 return;
             }
-            if ("$f" =~ m{^(?:t/|inc/)}) {
+            if ("$f" =~ m{^(?:t/|inc/|sample/)}) {
                 # msg("skip $f, by $no_index");
                 return;
             }
