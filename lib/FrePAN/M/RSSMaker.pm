58fe59257dbef60b42ee32677ce3a23819b27458
tokuhirom <tokuhirom@gmail.com>
2010-03-16 12:09:29 +0900

    make rss order by released date.

--- lib/FrePAN/M/RSSMaker.pm
+++ lib/FrePAN/M/RSSMaker.pm
@@ -25,7 +25,7 @@ sub generate {
     $feed->title('Yet Another CPAN Recent Changes');
     $feed->link('http://frepan.64p.org/');
     my $iter = $c->db->search_by_sql(
-        q{SELECT dist.name, dist.author, dist.version, dist.path, dist.abstract, changes.body AS diff, dist.released FROM dist LEFT JOIN changes ON (changes.dist_id = dist.dist_id) ORDER BY dist.dist_id DESC LIMIT 10}
+        q{SELECT dist.name, dist.author, dist.version, dist.path, dist.abstract, changes.body AS diff, dist.released FROM dist LEFT JOIN changes ON (changes.dist_id = dist.dist_id) ORDER BY dist.released DESC LIMIT 10}
     );
     while (my $row = $iter->next) {
         $feed->add_entry(do {
