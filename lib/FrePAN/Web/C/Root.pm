451c53e18fe3f585847f65cd8788c4809e61699c
tokuhirom <tokuhirom@gmail.com>
2010-03-22 13:18:19 +0900

    reduce the sql.

--- lib/FrePAN/Web/C/Root.pm
+++ lib/FrePAN/Web/C/Root.pm
@@ -7,13 +7,13 @@ sub index {
     my $rows_per_page = 20;
 
     my @entries = db->search_by_sql(
-        "SELECT dist.*, changes.body AS diff FROM dist LEFT JOIN changes ON (changes.dist_id=dist.dist_id) ORDER BY released DESC LIMIT @{[ $rows_per_page + 1 ]} OFFSET @{[ $rows_per_page*($page-1) ]}"
+        "SELECT dist.*, changes.body AS diff, meta_author.email AS email FROM dist LEFT JOIN changes ON (changes.dist_id=dist.dist_id) LEFT JOIN meta_author ON ( meta_author.pause_id=dist.author) ORDER BY released DESC LIMIT @{[ $rows_per_page + 1 ]} OFFSET @{[ $rows_per_page*($page-1) ]}"
     );
     my $has_next =  ($rows_per_page+1 == @entries);
     if ($has_next) { pop @entries }
 
     for (@entries) {
-        $_->{gravatar_url} = model('CPAN')->pause_id2gravatar_url($_->author);
+        $_->{gravatar_url} = model('CPAN')->email2gravatar_url($_->email);
     }
     render("index.mt", \@entries, $page, $has_next);
 }
