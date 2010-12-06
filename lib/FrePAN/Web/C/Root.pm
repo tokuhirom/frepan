5067d41c8b3ac98e476572b95e3497ac2d1f6b53
tokuhirom <tokuhirom@gmail.com>
2010-12-07 00:49:51 +0900

    refactoring

--- lib/FrePAN/Web/C/Root.pm
+++ lib/FrePAN/Web/C/Root.pm
@@ -12,9 +12,11 @@ sub index {
     $page =~ /^[0-9]+$/ or die "bad page number: $page";
     my $rows_per_page = 20;
 
-    my $dbh = $c->db->dbh;
-    my $entries = $dbh->selectall_arrayref(
-        "SELECT SQL_CACHE dist.dist_id, dist.name, dist.author, dist.version, dist.abstract, dist.released FROM dist ORDER BY released DESC LIMIT @{[ $rows_per_page + 1 ]} OFFSET @{[ $rows_per_page*($page-1) ]}",
+    my $entries = $c->dbh->selectall_arrayref(
+        "SELECT SQL_CACHE dist.dist_id, dist.name, dist.author, dist.version, dist.abstract, dist.released
+        FROM dist
+        ORDER BY released DESC
+        LIMIT @{[ $rows_per_page + 1 ]} OFFSET @{[ $rows_per_page*($page-1) ]}",
         { Slice => {} }
     );
 
@@ -22,7 +24,7 @@ sub index {
     my $pause_id2email = $c->memcached->get_or_set_cb(
         "pause_id2email:2" => 24 * 60 * 60 => sub {
             +{ map { $_->[0] => $_->[1] } @{
-                $dbh->selectall_arrayref(
+                $c->dbh->selectall_arrayref(
                     q{SELECT pause_id, email FROM meta_author})
               } };
         }
@@ -37,7 +39,7 @@ sub index {
         my ($sql, @bind) = sql_interp q{SELECT dist_id, body FROM changes WHERE dist_id IN }, \@dist_ids;
         my %rows =
           ( map { $_->[0] => $_->[1] }
-              @{ $dbh->selectall_arrayref( $sql, {}, @bind ) } );
+              @{ $c->dbh->selectall_arrayref( $sql, {}, @bind ) } );
         for my $entry (@$entries) {
             $entry->{diff} = $rows{$entry->{dist_id}};
         }
