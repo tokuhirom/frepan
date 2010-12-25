ba22c4826b531679b966e16764e68bb90b7d51b2
tokuhirom <tokuhirom@gmail.com>
2010-12-25 17:57:04 +0900

    simplified code

--- htdocs/static/js/frepan.js
+++ htdocs/static/js/frepan.js
@@ -1,19 +1,24 @@
 $(function () {
-    $('.gravatar_container').each(function (i, elem) {
-        elem = $(elem);
-        elem.html(
-            $(document.createElement('img'))
-                .attr('src' , "http://www.gravatar.com/avatar/" + CybozuLabs.MD5.calc(elem.attr('email')) + "?d=http://st.pimg.net/tucs/img/who.png")
-                .attr('width', 80)
-                .attr('height', 80)
-                .attr('alt', $(elem).attr('email'))
-        );
-    });
+    // run on every page
+    (function () {
+        $('.gravatar_container').each(function (i, elem) {
+            elem = $(elem);
+            elem.html(
+                $(document.createElement('img'))
+                    .attr('src' , "http://www.gravatar.com/avatar/" + CybozuLabs.MD5.calc(elem.attr('email')) + "?d=http://st.pimg.net/tucs/img/who.png")
+                    .attr('width', 80)
+                    .attr('height', 80)
+                    .attr('alt', $(elem).attr('email'))
+            );
+        });
+    })();
 
     // dist page
     dispatcher('^/~.+/.+/', function () {
-        $('pre').addClass('prettyprint').addClass('lang-perl');
-        prettyPrint();
+        require(['/static/prettify/prettify.js'], function () {
+            $('pre').addClass('prettyprint').addClass('lang-perl');
+            prettyPrint();
+        });
     });
 
     // http://tech.kayac.com/archive/javascript-url-dispatcher.html
