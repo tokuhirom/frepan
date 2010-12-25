4541ab142de087f1c7952eecb2bb2a713c7b1451
tokuhirom <tokuhirom@gmail.com>
2010-12-25 17:53:01 +0900

    simplified js code

--- htdocs/static/js/frepan.js
+++ htdocs/static/js/frepan.js
@@ -9,4 +9,22 @@ $(function () {
                 .attr('alt', $(elem).attr('email'))
         );
     });
+
+    // dist page
+    dispatcher('^/~.+/.+/', function () {
+        $('pre').addClass('prettyprint').addClass('lang-perl');
+        prettyPrint();
+    });
+
+    // http://tech.kayac.com/archive/javascript-url-dispatcher.html
+    function dispatcher (path, func) {
+        dispatcher.path_func = dispatcher.path_func || []
+        if (func) return dispatcher.path_func.push([path, func]);
+        for(var i = 0, l = dispatcher.path_func.length; i < l; ++i) {
+            var func = dispatcher.path_func[i];
+            var match = path.match(func[0]);
+            match && func[1](match);
+        };
+    };
+    dispatcher(location.pathname);
 });
