62d06b9574d5be58e4ebad42863205fbf3d1464e
tokuhirom <tokuhirom@gmail.com>
2010-12-30 11:11:14 +0900

    added /perldoc? link

--- lib/FrePAN/Pod/POM/View/HTML.pm
+++ lib/FrePAN/Pod/POM/View/HTML.pm
@@ -263,7 +263,7 @@ sub view_seq_link {
 
 sub view_seq_link_transform_path {
     my ( $self, $page ) = @_;
-    return "http://search.cpan.org/perldoc?" . URI::Escape::uri_escape($page);
+    return "/perldoc?" . URI::Escape::uri_escape($page);
 }
 
 sub make_href {
