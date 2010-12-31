37d0406b04f2c725be0c2939954578129a0626a6
tokuhirom <tokuhirom@gmail.com>
2010-12-31 23:49:19 +0900

    oops. bug fixed around formatter

--- lib/FrePAN/M/Formatter.pm
+++ lib/FrePAN/M/Formatter.pm
@@ -3,11 +3,12 @@ use strict;
 use warnings;
 use utf8;
 use Smart::Args;
+use Text::Xslate::Util qw/mark_raw/;
 
 sub format {
     args_pos my $src;
     $src =~ s!\n!<br />!g;
-    return $src;
+    return mark_raw($src);
 }
 
 1;
