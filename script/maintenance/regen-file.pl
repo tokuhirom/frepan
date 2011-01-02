04637fa59ade0661f6fc770957d0847b46ca1a96
tokuhirom <tokuhirom@gmail.com>
2011-01-02 18:27:14 +0900

    revert

--- script/maintenance/regen-file.pl
+++ script/maintenance/regen-file.pl
@@ -8,7 +8,7 @@ use Log::Minimal;
 
 my $c = FrePAN->bootstrap();
 
-my $iter = $c->db->search('dist' => { }, {order_by => {dist_id => 'DESC'}});
+my $iter = $c->db->search('dist' => { old => 0 }, {order_by => {dist_id => 'DESC'}});
 while (my $dist = $iter->next) {
     my $dir = $dist->extracted_dir();
     unless (-d $dir) {
@@ -28,6 +28,6 @@ while (my $dist = $iter->next) {
         dir  => $dist->extracted_dir(),
         c    => $c,
     );
-#   $dist->insert_to_fts();
+    $dist->insert_to_fts();
 }
 
