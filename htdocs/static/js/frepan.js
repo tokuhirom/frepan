3dcd808584b8be13828387565d3d59b683d4cc59
tokuhirom <tokuhirom@gmail.com>
2011-01-02 09:35:04 +0900

    remove trash

--- htdocs/static/js/frepan.js
+++ htdocs/static/js/frepan.js
@@ -1,17 +1,6 @@
 $(function () {
     // run on every page
     (function () {
-        $('.gravatar_container').each(function (i, elem) {
-            elem = $(elem);
-            elem.html(
-                $(document.createElement('img'))
-                    .attr('src' , "http://www.gravatar.com/avatar/" + CybozuLabs.MD5.calc(elem.attr('email')) + "?d=http://st.pimg.net/tucs/img/who.png")
-                    .attr('width', 80)
-                    .attr('height', 80)
-                    .attr('alt', $(elem).attr('email'))
-            );
-        });
-
         var ajaxManager = $.manageAjax.create('search', {
             queue: 'clear', 
             cacheResponse: false,
