30c0f565860fc7beaeef22cc110db6d9e9e4abe2
tokuhirom <tokuhirom@gmail.com>
2011-01-15 17:23:45 +0900

    remove older stuff

--- lib/FrePAN/M/CPANDB.pm
+++ lib/FrePAN/M/CPANDB.pm
@@ -2,38 +2,4 @@ package FrePAN::M::CPANDB;
 use common::sense;
 use Amon2::Declare;
 
-sub new {
-    my ($class, $args) = @_;
-    bless {%$args}, $class;
-}
-
-sub mk_author {
-    my ($self, ) = @_;
-    my $txn = db->txn_begin();
-
-    my @rows;
-
-    db->dbh->do(q{DROP TABLE IF EXISTS author_old});
-    db->dbh->do(q{CREATE TABLE author_tmp LIKE author});
-
-    my $insert = sub {
-        db->bulk_insert(
-            'author_tmp' => \@rows,
-        );
-        @rows = ();
-    };
-
-    while (<$fh>) {
-        my ($pauseid, $fullname, $email) = /^alias (\S+)\s*"([^<]+)<([^>]+)>"/;
-        die "cannot parse:$_" unless $email;
-        push @rows, {pause_id => $pauseid, fullname => $fullname, email => $email};
-        $insert->() if @rows > 1000;
-    }
-    $insert->() if @rows;
-
-    db->dbh->do(q{RENAME TABLE author_tmp TO author, author TO author_old;});
-
-    $txn->commit;
-}
-
 1;
