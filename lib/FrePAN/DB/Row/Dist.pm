f09187084b6419b22abcc2459123ef894ba5c1c4
tokuhirom <tokuhirom@gmail.com>
2011-01-22 18:16:00 +0900

    make 'perl' itself as always authorized

--- lib/FrePAN/DB/Row/Dist.pm
+++ lib/FrePAN/DB/Row/Dist.pm
@@ -173,6 +173,7 @@ sub index_file {
 
     my $authorized = FrePAN::M::CPANDB->is_authorized(
         c        => $c,
+        dist     => $self,
         package  => $parser->package,
         pause_id => $self->author,
     );
@@ -224,7 +225,7 @@ sub insert_to_fts {
         # is this latest release?
         my ($latest_version) =
           reverse
-          sort          { eval { version->parse($a) } <=> eval { version->parse($b) } }
+          sort          { (eval { version->parse($a) } || 0) <=> (eval { version->parse($b) } || 0) }
           grep { $_ !~ /-withoutworldwriteables$/ }
           grep { $_ !~ /_/ }
           map { $_->version } @old_dists;
