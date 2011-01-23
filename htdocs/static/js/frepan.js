5fd9a239b0fc6d8f19c1052a470a19b73c3df3b6
tokuhirom <tokuhirom@gmail.com>
2011-01-23 19:23:05 +0900

    use pushstate!

--- htdocs/static/js/frepan.js
+++ htdocs/static/js/frepan.js
@@ -1,4 +1,5 @@
 $(function () {
+    console.log("RELOAD");
     // run on every page
     (function () {
         var ajaxManager = $.manageAjax.create('search', {
@@ -31,6 +32,49 @@ $(function () {
                 });
             }
         });
+
+        if (window.history.pushState) {
+            var ajax_part_load = function (url, pushit) {
+                console.log("LOADING: " + url);
+                $.ajax({
+                    url: url,
+                    data: {slide: 1},
+                    type: 'GET',
+                    cache: false,
+                    dataType: 'html',
+                    success: function (src) {
+                        if (pushit) {
+                            history.pushState({path:url}, $('title', src), url);
+                        } else {
+                            history.replaceState({path:url}, $('title', src), url);
+                        }
+
+                        $('#Content').replaceWith($('#Content', src)).scrollTop();
+                        dispatcher(url);
+                    }
+                });
+            };
+            $('#Content a').live('click', function () {
+                var e = $(this);
+                var href = e.attr('href');
+                if (
+                    href.match(/^https?:\/\//) // absolute uri
+                    || href.match(/^\/src\//)  // source page
+                ) {
+                    return true;
+                }
+                ajax_part_load(href, true);
+                return false;
+            });
+            window.addEventListener('popstate', function(event) {
+                if (event && event.state) {
+                    ajax_part_load(event.state.path, false);
+                    return false;
+                } else {
+                    return true;
+                }
+            }, false);
+        }
 
         // mylingual
         var lang = navigator.language;
