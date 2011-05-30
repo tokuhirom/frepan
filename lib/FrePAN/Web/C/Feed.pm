aea22733190810cf08b65ed4bc402f370b4cd394
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-31 03:20:33 +0900

    oops

--- lib/FrePAN/Web/C/Feed.pm
+++ lib/FrePAN/Web/C/Feed.pm
@@ -7,9 +7,17 @@ use FrePAN;
 use autodie;
 
 sub index {
+    my $c = shift;
     my $fname = FrePAN->config->{'M::RSSMaker'}->{'path'} // die "Missing configuration for rss file";
     open my $fh, '<', $fname or die "Cannot open rss file: $fname";
-    return [200, ['Content-Type' => 'text/xml;charset=utf-8', 'Content-Length' => -s $fname], $fh];
+    return $c->create_response(
+        200,
+        [
+            'Content-Type'   => 'text/xml;charset=utf-8',
+            'Content-Length' => -s $fname
+        ],
+        $fh
+    );
 }
 
 1;
