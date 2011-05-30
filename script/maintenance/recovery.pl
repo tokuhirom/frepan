7fc8c6cdc530be8ec3f8a52ad6d04f7172c81f1a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-31 02:14:03 +0900

    recovery script

--- script/maintenance/recovery.pl
+++ script/maintenance/recovery.pl
@@ -2,15 +2,10 @@
 use strict;
 use warnings;
 use utf8;
-use Web::Scraper;
-use Log::Minimal;
-use URI;
-use 5.10.1;
+use Web::Query;
+use 5.010001;
 
-my $s = scraper {
-    process 'a' => 'links[]' => '@href';
-}->scrape(URI->new('http://friendfeed.com/cpan'));
-my @u = grep m{^http://cpan}, map { $_->as_string } @{$s->{links}};
-for (@u) {
-    say qq{sudo -u frepan sh -c 'PLACK_ENV=production /usr/local/app/perl/bin/perl -Ilib script/maintenance/inject-standalone.pl $_'};
+for (grep m{^http://cpan},
+  map { $_->attr('href') } wq('http://friendfeed.com/cpan')->find('a')) {
+    say qq{perl -Ilib script/maintenance/inject-standalone.pl $_};
 }
