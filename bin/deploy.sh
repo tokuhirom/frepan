23ccb918ea5239e284079dce920c9f4383507ab4
tokuhirom <tokuhirom@gmail.com>
2012-09-14 09:21:38 +0900

    do not use plack

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/zsh
 DOMAIN=frepan.org
 
-if [ ! -f /etc/init/$DOMAIN.conf ]; then
-    sudo ln -s `pwd`/etc/init/$DOMAIN.conf /etc/init/$DOMAIN.conf
-fi
-
 if [ ! -f /etc/nginx/sites-enabled/$DOMAIN.conf ]; then
     sudo ln -s `pwd`/etc/httpd/$DOMAIN.conf /etc/nginx/sites-enabled/$DOMAIN.conf
 fi
 
 sudo /etc/init.d/nginx reload
-sudo initctl reload-configuration
-sudo stop $DOMAIN
-sudo start $DOMAIN
 
 echo "--------------------------"
 echo "Deployment finished"
